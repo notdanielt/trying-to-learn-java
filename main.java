e67a5c0a2ac2b6c425ef74b8e97165e48be7e990
-Danielt- <164779581+notdanielt@users.noreply.github.com>
2025-01-25 12:47:23 -0500

    yes

--- main.java
+++ main.java
@@ -1,10 +1,18 @@
-public class main {
+import java.io.*;
+import java.lang.Thread;
+public class Main {
   public static void main(String[] args)
   {
     int variable_because_yes = 0;
-    while (True) {
-      variable_because_yes += 1;
-      System.out.println(variable_because_yes);
+    while (true) {
+      try {
+        variable_because_yes += 1;
+        System.out.println(variable_because_yes);
+        Thread.sleep(50/3);
+      }
+      catch (Exception e) {
+          System.out.println(e);
+      }
     }
   }
 }
