22fb191108b28c6ac6eea5ef39d486e75dc0c611
-Danielt- <164779581+notdanielt@users.noreply.github.com>
2025-01-25 12:34:27 -0500

    when the

---
+++ main.java
@@ -0,0 +1,10 @@
+public class main {
+  public static void main(String[] args)
+  {
+    int variable_because_yes = 0;
+    while (True) {
+      variable_because_yes += 1;
+      System.out.println(variable_because_yes);
+    }
+  }
+}
